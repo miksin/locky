09295873cd54c71fc7f2925e8ec4bea1cb27cb35
miksin <contact@miksin.art>
2025-01-15 21:26:31 +0900

    generate

---
+++ src/lib/Generator.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  import { Charset, generate } from "./generate";
+
+  type Options = {
+    lowercase: boolean;
+    uppercase: boolean;
+    digits: boolean;
+    symbols: boolean;
+  }
+  let options: Options = $state({
+    lowercase: true,
+    uppercase: true,
+    digits: true,
+    symbols: true
+  })
+  let charsets: string[] = $derived([
+    options.lowercase ? Charset.Lowercase : '',
+    options.uppercase ? Charset.Uppercase : '',
+    options.digits ? Charset.Digits : '',
+    options.symbols ? Charset.Symbols : '',
+  ].filter(v => v !== ''))
+
+  let pw = $state('')
+  const onClick = () => {
+    pw = generate(10, charsets)
+  }
+</script>
+
+<div>
+  <p>{pw}</p>
+  <div>
+    <label>
+      <input type="checkbox" bind:checked={options.lowercase} disabled={charsets.length === 1 && options.lowercase} />
+      lowercase
+    </label>
+    <label>
+      <input type="checkbox" bind:checked={options.uppercase} disabled={charsets.length === 1 && options.uppercase} />
+      uppercase
+    </label>
+    <label>
+      <input type="checkbox" bind:checked={options.digits} disabled={charsets.length === 1 && options.digits} />
+      digits
+    </label>
+    <label>
+      <input type="checkbox" bind:checked={options.symbols} disabled={charsets.length === 1 && options.symbols} />
+      symbols
+    </label>
+  </div>
+  <button onclick={onClick}>
+    generate
+  </button>
+</div>
