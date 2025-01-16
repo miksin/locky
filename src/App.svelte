18492296662c278faf308dafb27aad12648ac123
miksin <contact@miksin.art>
2025-01-16 21:38:15 +0900

    logo

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
   import Generator from "./lib/Generator.svelte";
+  import Logo from "./lib/Logo.svelte";
 </script>
 
-<main class="mx-auto flex w-full max-w-96 flex-col gap-4 p-4">
-  <h1 class="prose prose-2xl">Locky</h1>
-  <h3 class="prose prose-l">Password Generator</h3>
+<main class="mx-auto flex min-h-dvh w-full max-w-96 flex-col gap-4 p-4 md:py-8">
+  <div class="flex flex-col">
+    <Logo />
+    <h3 class="prose-l prose">Password Generator</h3>
+  </div>
   <Generator />
+  <p class="prose ml-auto mt-auto">
+    Made by <a href="https://github.com/miksin">@miksin</a>
+  </p>
 </main>
---
+++ src/lib/Logo.svelte
@@ -0,0 +1,11 @@
+<div class="logo w-3/4 bg-neutral"></div>
+
+<style>
+  .logo {
+    mask-image: url("/logo.svg");
+    mask-repeat: no-repeat;
+    mask-position: 0 0;
+    mask-size: 100%;
+    aspect-ratio: 137 / 48;
+  }
+</style>
